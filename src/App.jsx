344d51ee1ae4f8911aa25e75b7876a5ab12616bf
ShaikRiyazuddin <shaikriyaz00723@gmail.com>
2022-06-03 19:29:27 +0530

    Latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import {Counter} from "./components/counter";
-import {Todo} from "./components/todo"
+import {Todo} from "./components/todo";
+import {User} from "./components/form"
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <Todo />
+      {/* <Counter />
+      <Todo /> */}
+      <User />
     </div>
   );
 }
